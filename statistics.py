e513b32f26b896435ba0bf229743de490c54e173
romaintailhurat <romain.tailhurat@gmail.com>
2015-10-14 18:14:56 +0200

    quantile and mode

--- statistics.py
+++ statistics.py
@@ -2,6 +2,7 @@
 Chapter 5
 """
 from __future__ import division
+from collections import Counter
 
 def mean(x):
     return sum(x) / len(x)
@@ -19,6 +20,17 @@ def median(v):
         hi = midpoint
         return (sorted_v[lo] + sorted_v[hi]) // 2
 
+def quantile(x,p):
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+def mode(x):
+    """ most common value(s) """
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [ x_i for x_i, count in counts.iteritems() if count == max_count ]
+
 ## ----- TESTS
 
-print(median([1,2,2,56]))
+print(quantile([1,2,2,56], 0.5))
+print(mode([1,2,2,56]))
