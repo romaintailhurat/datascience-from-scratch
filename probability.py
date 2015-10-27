86806848f30a92fad4639c73f138d5a0617c1072
romaintailhurat <romain.tailhurat@gmail.com>
2015-10-27 14:07:11 +0100

    Starting proba

---
+++ probability.py
@@ -0,0 +1,35 @@
+"""
+Chapter 6
+
+Maths:
+- independent events P(E,F)=P(E)P(F)
+- "E knowing F" P(E|F)=P(E,F)/P(F) <=> P(E,F)=P(E|F)P(F)
+"""
+from __future__ import division
+import random
+
+
+def random_kid():
+    return random.choice(["boy", "girl"])
+
+both_girls = 0
+older_girl = 0
+either_girl = 0
+
+random.seed(0)
+
+for _ in range(10000):
+    younger = random_kid()
+    older = random_kid()
+    if older == "girl":
+        older_girl += 1
+    if older == "girl" and younger == "girl":
+        both_girls += 1
+    if older == "girl" or younger == "girl":
+        either_girl += 1
+
+#print "P(both | older):", both_girls / older_girl
+#print "P(both | either): ", both_girls / either_girl
+
+def uniform_pdf(x):
+    return 1 if x => 0 and x < 1 else O
