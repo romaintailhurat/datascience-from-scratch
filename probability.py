e555c11af10cc2610d2316c919cbefea4a0decfa
romaintailhurat <romain.tailhurat@gmail.com>
2015-10-28 13:48:33 +0100

    Uniform and normal distribution, some plotting

--- probability.py
+++ probability.py
@@ -6,7 +6,9 @@ Maths:
 - "E knowing F" P(E|F)=P(E,F)/P(F) <=> P(E,F)=P(E|F)P(F)
 """
 from __future__ import division
+from matplotlib import pyplot as plt
 import random
+import math
 
 
 def random_kid():
@@ -31,5 +33,44 @@ for _ in range(10000):
 #print "P(both | older):", both_girls / older_girl
 #print "P(both | either): ", both_girls / either_girl
 
+# ----- Uniform distribution
+
 def uniform_pdf(x):
-    return 1 if x => 0 and x < 1 else O
+    """
+    Probability Density Function (PDF) for the uniform distribution.
+    """
+    return 1 if x >= 0 and x < 1 else O
+
+def uniform_cdf(x):
+    """
+    Cumulative Density Function (CDF)
+    """
+    if x < 0: return 0
+    elif x < 1: return x
+    else : return 1
+
+# ----- Normal distribution
+
+def normal_pdf(x, mu=0, sigma=0):
+    """
+    Normal distribution is the classic bell-curved distribution.
+    Mu is the mean.
+    Sigma is the standard deviation.
+    When mu = 0 and sigma = 2, it's called the standard normal distribution.
+    """
+    sqrt_two_pi = math.sqrt(2 * math.pi)
+    return (math.exp(- (x - mu) ** 2 / 2 / sigma ** 2)) / (sqrt_two_pi * sigma)
+
+# ----- Plotting
+
+def plotting_normal_pdf() :
+    xs = [x / 10.0 for x in range(-50, 50)]
+    plt.plot(xs,[normal_pdf(x,sigma=1) for x in xs],'-',label='mu=0,sigma=1')
+    plt.plot(xs,[normal_pdf(x,sigma=2) for x in xs],'--',label='mu=0,sigma=2')
+    plt.plot(xs,[normal_pdf(x,sigma=0.5) for x in xs],':',label='mu=0,sigma=0.5')
+    plt.plot(xs,[normal_pdf(x,mu=-1, sigma=1) for x in xs],'-.',label='mu=-1,sigma=1')
+    plt.legend()
+    plt.title("Various Normal pdfs")
+    plt.show()
+
+# ----- Tests
