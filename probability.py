fd7e1be3cdaaf121bc08c49b44fa33bfd9fef652
romaintailhurat <romain.tailhurat@gmail.com>
2015-11-05 17:17:52 +0100

    Adding normal cumulative distribution function and the inverse

--- probability.py
+++ probability.py
@@ -61,6 +61,29 @@ def normal_pdf(x, mu=0, sigma=0):
     sqrt_two_pi = math.sqrt(2 * math.pi)
     return (math.exp(- (x - mu) ** 2 / 2 / sigma ** 2)) / (sqrt_two_pi * sigma)
 
+def normal_cdf(x, mu=0, sigma=1):
+    return (1 + math.erf( (x - mu) / math.sqrt(2) / sigma)) / 2
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    """find approximate inverse using binary search"""
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    ￼low_z, low_p = -10.0, 0 # normal_cdf(-10) is (very close to) 0
+    hi_z,  hi_p  =  10.0, 1 # normal_cdf(10)  is (very close to) 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2 # consider the midpoint
+        mid_p = normal_cdf(mid_z) # and the cdf's value there
+        if mid_p < p:
+            # midpoint is still too low, search above it
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+            # midpoint is still too high, search below it
+            hi_z, hi_p = mid_z, mid_p
+        else:
+            break
+        return mid_z
+
 # ----- Plotting
 
 def plotting_normal_pdf() :
@@ -73,4 +96,15 @@ def plotting_normal_pdf() :
     plt.title("Various Normal pdfs")
     plt.show()
 
+def plotting_normal_cdf():
+    xs = [x / 10.0 for x in range(-50, 50)]
+    plt.plot(xs,[normal_cdf(x,sigma=1) for x in xs],'-',label='mu=0,sigma=1')
+    plt.plot(xs,[normal_cdf(x,sigma=2) for x in xs],'--',label='mu=0,sigma=2')
+    plt.plot(xs,[normal_cdf(x,sigma=0.5) for x in xs],':',label='mu=0,sigma=0.5')
+    plt.plot(xs,[normal_cdf(x,mu=-1) for x in xs],'-.',label='mu=-1,sigma=1')
+    plt.legend(loc=4) # bottom right
+    plt.title("Various Normal cdfs")
+    plt.show()
+
 # ----- Tests
+plotting_normal_cdf()
